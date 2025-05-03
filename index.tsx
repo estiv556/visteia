caf9d67dbe92a0f3589974b48bfdb56cc9abf81d
estiv556 <ullttimameta@gmail.com>
2025-05-03 12:03:35 -0500

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,9 @@
+import PromptForm from "@/components/PromptForm";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-white flex flex-col items-center justify-center">
+      <PromptForm />
+    </main>
+  );
+}
